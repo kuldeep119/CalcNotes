96d8311e180d11a4d40c1edf1b39f4179c39525f
Kuldeep <kuldeepsingh41204@gmail.com>
2025-08-21 16:12:48 +0530

    Initial commit

---
+++ CalcNotes/CalcNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcNotesApp.swift
+//  CalcNotes
+//
+//  Created by Kuldeep singh on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CalcNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
